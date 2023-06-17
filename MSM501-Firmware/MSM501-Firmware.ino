9247170d8d82e3ac856021a661575d18b394cb3c
ndornseif <5382403+ndornseif@users.noreply.github.com>
2023-06-17 19:28:47 +0200

    Added calibration factor for ref clock

--- MSM501-Firmware/MSM501-Firmware.ino
+++ MSM501-Firmware/MSM501-Firmware.ino
@@ -28,8 +28,13 @@ const byte StatusLED = 18;
 //Pin connected to the conditioned input signal. Currently unused.
 const byte ConditionedSignal = 19;
 
+//Correction factor for 1MHz internal clock: +6.8e6
+//This correction factor was measured for a particular device.
+//If you dont want to measure your correction factor use zero.
+const unsigned long correctionFactor = 6.8e6;
+
 //The frequency of the reference clock in uHz.
-const unsigned long long refClockFrequency = 1e6 * 1e6; //1 MHz 
+const unsigned long long refClockFrequency = (1e6 * 1e6) + correctionFactor; //1 MHz * 1e6 uHz/Hz + correction factor
 
 //Default time for a single measurment in ms.
 const unsigned long defaultGateTime = 60000;
